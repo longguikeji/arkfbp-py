b93fa273851c196a6ca191e11f51f548aacae8a4
97390483@qq.com <97390483@qq.com>
2020-08-28 19:41:08 +0800

    feat(run flow): add header info

--- arkfbp/common/django/management/commands/runflow.py
+++ arkfbp/common/django/management/commands/runflow.py
@@ -15,10 +15,11 @@ class Command(BaseCommand):
         path = options.get('flow')
         input = options.get('input')
         http_method = options.get('http_method', None)
+        header = options.get('header', None)
         try:
             clz = importlib.import_module(f'{path}.main')
             instance = clz.Main()
-            cli_start_flow(instance, input, http_method=http_method)
+            cli_start_flow(instance, input, http_method=http_method, header=header)
         except ModuleNotFoundError:
             raise CommandError('Run failed, Invalid flow.')
 
@@ -26,6 +27,7 @@ class Command(BaseCommand):
         parser.add_argument('--flow', type=str, help='Specifies the import path for the target flow name.')
         parser.add_argument('--input', type=str, help='Input data at the beginning of the flow.')
         parser.add_argument('--http_method', type=str, help='HTTP method of flow.')
+        parser.add_argument('--header', type=str, help='HTTP method of flow.')
 
 
 def cli_start_flow(flow, inputs, *args, **kwargs):
@@ -33,22 +35,24 @@ def cli_start_flow(flow, inputs, *args, **kwargs):
     if isinstance(flow, ViewFlow):
         http_method = kwargs.get('http_method')
         content_type = kwargs.get('content_type', 'application/json')
+        header = kwargs.get('header')
         if not http_method:
-            raise CommandError('缺少请求访问方式参数: --http_method')
+            raise CommandError('Lack of parameter: --http_method')
+        if header:
+            header = json.loads(header)
         # 构造 WSGIRequest 对象
         request_factory = RequestFactory()
-
         if http_method.lower() in ['get']:
             _inputs = json.loads(inputs)
             query_string = ''
             for key, value in _inputs.items():
                 query_string += f'{key}={value}&'
             query_string = f'?{query_string[:-1]}'
-            request = request_factory.generic(http_method, query_string, content_type=content_type)
+            request = request_factory.generic(http_method, query_string, content_type=content_type, **header)
         elif http_method.lower() in ['post', 'put', 'patch', 'delete']:
-            request = request_factory.generic(http_method, '', data=inputs, content_type=content_type)
+            request = request_factory.generic(http_method, '', data=inputs, content_type=content_type, **header)
         else:
-            raise CommandError('无效的请求访问方式参数: --http_method')
+            raise CommandError('Invalid parameter: --http_method')
 
         inputs = ViewFlow.convert_request(request)
 
