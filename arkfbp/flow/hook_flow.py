2a30a27401cff2a1635cfad3fb5d0004a6281c0f
97390483@qq.com <97390483@qq.com>
2020-09-07 19:54:59 +0800

    feat: hook support shutdown flow

--- arkfbp/flow/hook_flow.py
+++ arkfbp/flow/hook_flow.py
@@ -1,3 +1,4 @@
+from django.http import JsonResponse, HttpResponse
 from django.utils.deprecation import MiddlewareMixin
 
 from ..flow import Flow
@@ -28,6 +29,9 @@ class GlobalHookFlow(MiddlewareMixin, Flow):
                 self._before_route = True
     """
 
+    response_type = JsonResponse
+    response_status = 200
+
     def __init__(self, get_response=None):
         self.before_route = False
         self.before_flow = False
@@ -40,11 +44,11 @@ class GlobalHookFlow(MiddlewareMixin, Flow):
         return FlowExecuter.start_flow(self, request, *args, **kwargs) if hook_switch else None
 
     def process_request(self, request):
-        self.execute_hook(self.before_route, request)
+        return self.execute_hook(self.before_route, request)
 
     def process_view(self, request, view_func, view_args, view_kwargs):
         flow_class = view_func.__dict__.get('view_class', None)
-        self.execute_hook(self.before_flow, request, flow_class=flow_class)
+        return self.execute_hook(self.before_flow, request, flow_class=flow_class)
 
     def process_exception(self, request, exception):
         self.execute_hook(self.before_exception, request, exception=exception)
@@ -58,3 +62,21 @@ class GlobalHookFlow(MiddlewareMixin, Flow):
         Sets the global hook flow mount location,
         which can be overridden by subclasses
         """
+
+    def shutdown(self, outputs, **kwargs):
+        self.response_status = kwargs.get('response_status', self.response_status)
+        super().shutdown(outputs)
+
+    @property
+    def response(self):
+        try:
+            self._response = self.response_type(self.outputs, status=self.response_status)
+        except TypeError:
+            # 自动识别响应数据类型
+            if type(self.outputs) == str:
+                return HttpResponse(self.outputs, status=self.response_status)
+            if type(self.outputs) == int:
+                return HttpResponse(str(self.outputs), status=self.response_status)
+            if type(self.outputs) == dict:
+                return JsonResponse(self.outputs, status=self.response_status)
+        return self._response
