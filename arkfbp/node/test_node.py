10b7141c2b61c87d67f90fca4e4e7b6e6202915d
97390483@qq.com <97390483@qq.com>
2020-09-14 17:52:19 +0800

    feat(cli): run all flowtest by cli

--- arkfbp/node/test_node.py
+++ arkfbp/node/test_node.py
@@ -1,7 +1,8 @@
 import sys
+
+from arkfbp.flow.executer import FlowExecuter
 from django.test import TestCase
 
-from ..flow import executer
 from .base import Node
 
 
@@ -19,24 +20,25 @@ class TestNode(TestCase, Node):
         self.http_method = http_method
         self.content_type = content_type
         self.header = header
-        return executer.FlowExecuter.start_test_flow(self.flow, self.inputs, http_method=self.http_method, content_type=self.content_type, header=self.header)
+        return FlowExecuter.start_test_flow(self.flow, self.inputs, http_method=self.http_method,
+                                            content_type=self.content_type, header=self.header)
 
-    def run(self):    
+    def run(self):
         self.setUp()
 
         through = []
-        testList =list(filter(lambda m: m.startswith("test_") and callable(getattr(self, m)), dir(self)))
+        testList = list(filter(lambda m: m.startswith("test_") and callable(getattr(self, m)), dir(self)))
         for testcase in testList:
             try:
-                getattr(self,testcase)()
-                sys.stdout.write(testcase+ ' is ok  \n')
+                getattr(self, testcase)()
+                sys.stdout.write(testcase + ' is ok  \n')
                 through.append(testcase)
             except Exception as e:
                 sys.stdout.write(testcase + ' failed  \n')
             continue
 
         self.tearDown()
-        print('--------------------------------------------------------------------------------------------------------------------------   \n')
-        print('测试用例有：'+ str(len(testList)) +'\n')
-        print('通过用例数为：'+str(len(through)) +'\n')
-        print('失败用例数为：'+str(len(testList)-len(through)) +'\n')
+        print('-----------------------------------------\n')
+        print('测试用例有：' + str(len(testList)) + '\n')
+        print('通过用例数为：' + str(len(through)) + '\n')
+        print('失败用例数为：' + str(len(testList) - len(through)) + '\n')
